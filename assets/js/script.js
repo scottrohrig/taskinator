6b6fa1a477ea65ce3137aa3db2b93a214d1b1d81
Scott Rohrig <scott.rohrig@gmail.com>
2021-11-16 18:24:36 -0800

    capture form values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,25 @@
 var formEl = document.querySelector('#task-form');
 var taskList = document.querySelector('#tasks-to-do');
-var taskText = document.querySelector('input[name=task-name]');
+var taskText = document.querySelector('input[name="task-name"]');
+var taskType = document.querySelector('select[name="task-type"]');
 
 const createTaskHandler = function(event) {
     event.preventDefault();
 
+    // create list item
     var listItem = document.createElement('li');
     listItem.className = 'task-item';
-    listItem.textContent = taskText.value;
+
+    // create div to hold task info and add to list item
+    var taskInfo = document.createElement('div');
+    // assign class name
+    taskInfo.className = 'task-info';
+    // add html to div
+    taskInfo.innerHTML = "<h3 class='task-name'>" + taskText.value + "</h3><span class='task-type'>" + taskType.value + "</span>";
+
+    listItem.appendChild(taskInfo);
+
+    // add entire list item to list
     taskList.appendChild(listItem);
 }
 
